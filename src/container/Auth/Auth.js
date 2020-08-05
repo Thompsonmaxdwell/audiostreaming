a18877c1f43ca8cc0bb1ec297f9fb5adaaa83f5a
Thompsonmaxdwell <oghenefokethompson@gmail.com>
2020-08-05 01:44:13 +0000

    Starting

---
+++ src/container/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Hello</div>
+        );
+    }
+}
+ 
+export default Auth;
